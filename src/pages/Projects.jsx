17bbe0580396360c0af91ad2ace52f77c3617bf0
Komola Khidirova <komolakhidirova@gmail.com>
2024-12-16 15:34:43 +0500

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -3,9 +3,8 @@ import { projects } from '../helpers/projectsList'
 
 const Projects = () => {
 	return (
-		<>
+		<main className='section'>
 			<h2 className='title-1'>Projects</h2>
-			<main className='section'>
 				<div className='container'>
 					<ul className='projects'>
 						{projects.map((project, index) => {
@@ -21,7 +20,6 @@ const Projects = () => {
 					</ul>
 				</div>
 			</main>
-		</>
 	)
 }
 
