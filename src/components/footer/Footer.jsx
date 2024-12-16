267404527a68dab913b6f16770356cacdb2f3bbd
Komola Khidirova <komolakhidirova@gmail.com>
2024-12-16 12:45:07 +0500

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -10,12 +10,12 @@ const Footer = () => {
 				<div className='footer__wrapper'>
 					<ul className='social'>
 						<li className='social__item'>
-							<a href='https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile'>
+							<a href='https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile' target='_blank'>
 								<img src={linkedIn} alt='Link' />
 							</a>
 						</li>
 						<li className='social__item'>
-							<a href='https://github.com/komolakhidirova'>
+							<a href='https://github.com/komolakhidirova' target='_blank'>
 								<img src={gitHub} alt='Link' />
 							</a>
 						</li>
