bf07a0755aca81a87a64e4e7e21205cc031de7e6
Komola Khidirova <komolakhidirova@gmail.com>
2025-01-04 11:37:44 +0500

    Update BtnEducation.jsx

--- src/components/btnEducation/BtnEducation.jsx
+++ src/components/btnEducation/BtnEducation.jsx
@@ -1,1 +1,15 @@
+import './style.css'
+
+const Education = ({ img, text, link }) => {
+	return (
+		<div className='edu'>
+			<a href={link} target='_blank' rel='noreferrer'>
+			<img src={img} alt='education' className='edu__img' />
+			{text}
+			</a>
+		</div>
+	)
+}
+
+export default Education
 
